e035eb3ff2d2da003721cf19e4683e5fdb1c5acf
Bertik23 <39457484+Bertik23@users.noreply.github.com>
2020-10-27 18:38:21 +0100

    residue drawing

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ nameOfBonds = ["en","yn"]
 
 uhlovodik = "3,4-ethylnon-1,7-en"
 # uhlovodik = "2,3-methylbut-2-en"
-uhlovodik = "3-methyl-4-(2-methyl-3-(2-propyl)propyl)oktan"
+uhlovodik = "3-methyl-4-(2-(2-propylhexyl)propyl)oktan"
 
 class GetOutOfLoop(Exception):
     pass
@@ -67,78 +67,66 @@ def ifString(s):
     else:
         return ""
 
+
+
+def get_hydrocarbon(hydrocarbonInput):
+    name = hydrocarbonInput
+    carbons = {}
+    while True:
+        try:
+            for hc in hydrocarbons:
+                if hydrocarbonInput.endswith(hc):
+                    hCtype = hc
+                    raise GetOutOfLoop
+        except GetOutOfLoop:
+            break
+        hydrocarbonInput = hydrocarbonInput[:-1]
+    hydrocarbonInput = rstripIfEndingWith(name[:name.rfind(hCtype)] + name[name.rfind(hCtype)+len(hCtype):],"an")
+    # print(hydrocarbonInput)
+    hydrocarbonList = hydrocarbonInput.split("-")
+    #print(hydrocarbonList)
+    while "(" in "".join(map(ifString,hydrocarbonList)):
+        for i, p in enumerate(hydrocarbonList):
+            #print(i,p)
+            try:
+                if p.startswith("("):
+                    for i_, p_ in enumerate(reversed(hydrocarbonList[i+1:])):
+                        if p_.endswith(")"):
+                            fromTo = i,i+1+len(hydrocarbonList[i+1:])-i_
+                            hydrocarbonList.insert(i,Residue("-".join(hydrocarbonList[fromTo[0]:fromTo[1]]).lstrip("(").rstrip(")")))
+                            for i__, p__ in enumerate(hydrocarbonList[fromTo[0]+1:fromTo[1]+1]):
+                                hydrocarbonList.pop(fromTo[0]+1)
+            except Exception as e:
+                print(e)
+
+#    print(hydrocarbonList)
+
+    for carbon in range(1,hydrocarbons.index(hCtype)+2):
+        if carbon not in carbons.keys():
+            carbons[carbon] = []
+        for i, p_ in enumerate(hydrocarbonList):
+            try:
+                for p in p_.split(","):
+                    try:
+                        if int(p) == carbon:
+                            if type(hydrocarbonList[i+1]) != Residue:
+                                carbons[carbon].append(Residue(hydrocarbonList[i+1]))
+                            else:
+                                carbons[carbon].append(hydrocarbonList[i+1])
+                    except ValueError:
+                        pass
+            except AttributeError:
+                pass
+    return carbons, hCtype
+
+
+
+
 class HydroCarbon:
     def __init__(self, name):
         self.name = name
-        self.get_hydrocarbon()
+        self.carbons, self.type = get_hydrocarbon(self.name)
         self.numberOfCarbons = hydrocarbons.index(self.type)+1
-        # self.makePlan()
-
-    def get_hydrocarbon(self):
-        hydrocarbonInput = self.name
-        self.carbons = {}
-        while True:
-            try:
-                for hc in hydrocarbons:
-                    if hydrocarbonInput.endswith(hc):
-                        self.type = hc
-                        raise GetOutOfLoop
-            except GetOutOfLoop:
-                break
-            hydrocarbonInput = hydrocarbonInput[:-1]
-        hydrocarbonInput = rstripIfEndingWith(self.name[:self.name.rfind(self.type)] + self.name[self.name.rfind(self.type)+len(self.type):],"an")
-        # print(hydrocarbonInput)
-        hydrocarbonList = hydrocarbonInput.split("-")
-        print(hydrocarbonList)
-        while "(" in "".join(map(ifString,hydrocarbonList)):
-            for i, p in enumerate(hydrocarbonList):
-                #print(i,p)
-                try:
-                    if p.startswith("("):
-                        for i_, p_ in enumerate(reversed(hydrocarbonList[i+1:])):
-                            if p_.endswith(")"):
-                                fromTo = i,i+1+len(hydrocarbonList[i+1:])-i_
-                                hydrocarbonList.insert(i,Residue("-".join(hydrocarbonList[fromTo[0]:fromTo[1]]).lstrip("(").rstrip(")")))
-                                for i__, p__ in enumerate(hydrocarbonList[fromTo[0]+1:fromTo[1]+1]):
-                                    hydrocarbonList.pop(fromTo[0]+1)
-                except Exception as e:
-                    print(e)
-
-        print(hydrocarbonList)
-
-        for carbon in range(1,hydrocarbons.index(self.type)+2):
-            if carbon not in self.carbons.keys():
-                self.carbons[carbon] = []
-            for i, p_ in enumerate(hydrocarbonList):
-                try:
-                    for p in p_.split(","):
-                        try:
-                            if int(p) == carbon:
-                                if type(hydrocarbonList[i+1]) != Residue:
-                                    self.carbons[carbon].append(Residue(hydrocarbonList[i+1]))
-                                else:
-                                    self.carbons[carbon].append(hydrocarbonList[i+1])
-                        except ValueError:
-                            pass
-                except AttributeError:
-                    pass
-        print(self.carbons)
-
-
-    def makePlan(self):
-        self.carbons = {}
-        for c in range(1,self.numberOfCarbons+1):
-            if c not in self.carbons.keys():
-                self.carbons[c] = []
-            for r in self.residues:
-                if r[1] == c:
-                    self.carbons[c].append(Residue(r[0]))
-
-            for b in self.bonds:
-                if b[1] == c:
-                    self.carbons[c].append(b[0])
-
-        print(self.carbons)
 
     def draw(self):
         direction = (0,1)
@@ -166,12 +154,9 @@ class HydroCarbon:
                     nextDrawBond = True
                     nextDrawBondTimes = nameOfBonds.index(r)+1
 
-                try:
-                    if r.name in hydrocarbons:
-                        print(c, r, x,y)
-                        r.draw(draw, x,y, oldX, oldY, length, thickness, direction)
-                except AttributeError:
-                    pass
+                if type(r) == Residue:
+                    #print(c, r, x,y)
+                    r.draw(draw, x,y, oldX, oldY, length, thickness, direction, bondOfset)
 
             oldX, oldY = x,y
             x += length
@@ -186,20 +171,48 @@ class Residue:
     def __init__(self, name):
         self.name = name[:-2]
         try:
-            self.carbons = hydrocarbons.index(self.name)+1
+            self.carbonsNumber = hydrocarbons.index(self.name)+1
         except:
             pass
+        print(self.name)
+        self.carbons, self.type = get_hydrocarbon(self.name)
+        #print(self.carbons)
 
     
-    def draw(self, draw, inX,inY, inOldX, inOldY, length, width, direction):
+    def draw(self, draw, inX,inY, inOldX, inOldY, length, width, direction, bondOfset):
         if direction[0] == 0:
             direction = (positiveOrNagative(inY - inOldY),0)
         else:
             direction = (0,positiveOrNagative(inX - inOldX))
         x,y = inX, inY
         oldX, oldY = x,y
-        for c in range(self.carbons+1):
+        nextDrawBond = False
+        nextDrawBondTimes = 0
+        tmp = [0]
+        tmp.extend(self.carbons.keys())
+        for c in tmp:
+            #print(c)
+            draw.point((x,y), fill=(0,0,0))
             draw.line((x,y, oldX, oldY), fill=(0,0,0), width=width)
+
+            if nextDrawBond:
+                for i in range(1,nextDrawBondTimes+1):
+                    draw.line((oldX+oddEven(c)*(oddEven(i)*math.ceil(i/2)*bondOfset), oldY+(oddEven(i)*math.ceil(i/2)*bondOfset), x+oddEven(c)*(oddEven(i)*math.ceil(i/2)*bondOfset), y+(oddEven(i)*math.ceil(i/2)*bondOfset)),fill=(0,0,0), width=width)
+                nextDrawBond = False
+
+            try:
+                for r in self.carbons[c]:
+                    if r in nameOfBonds:
+                        nextDrawBond = True
+                        nextDrawBondTimes = nameOfBonds.index(r)+1
+
+                    elif type(r) == Residue:
+                        print(c, r, x,y)
+                        print(direction)
+                        r.draw(draw, x,y, oldX, oldY, length, width, direction, bondOfset)
+            except Exception as e:
+                print(e)
+
             oldX, oldY = x,y
             if direction[0] == 0:
                 x += length*direction[1]
