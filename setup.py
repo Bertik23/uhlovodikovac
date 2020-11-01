4722904289358b9399844e9135ba3c516d5243da
Bertik23 <39457484+Bertik23@users.noreply.github.com>
2020-11-01 14:52:16 +0100

    more pypi preparation

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="uhlovod", # Replace with your own username
+    name="uhlovodikovac", # Replace with your own username
     version="0.0.1",
     author="Bertik23",
     author_email="bertikxxiii@gmail.com",
@@ -12,11 +12,15 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/bertik23/uhlovodikovac",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        "Pillow ~= 7.0.0"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
+    package_dir={"":"src"}
 )
